038513bffc4f53c2a74ed936b05a3c390e5d2831
rahulthakur999@gmail.com <rahul.kumar@ordergenie.co.in>
2019-03-16 12:03:08 +0530

    [commited by Manoj]

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 var data1 = "This is the string";
 var data2 = "This is the new string";
 
+var data3 = "<h1>Edited by Majoj</h1>";
+
 for(text in data1){
-    console(text);
+    console.log(text);
+}
+
+for (let t in data3){
+    console.log(t);
 }
